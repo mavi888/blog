a3bebb79e3573bf62df5f3264756f77867cf9047
marcia.villalba <marcia.villalba@gmail.com>
2015-05-08 20:53:47 +0300

    Tutorial 1 - My first web s server

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+ 
+var hbs = require('hbs');
+var blogEngine = require('./blog');
+
+app.set('view engine', 'html');
+app.engine('html', hbs.__express);
+app.use(express.bodyParser());
+
+app.get('/', function(req, res) {
+ res.render('index',{title:"My Blog", entries:blogEngine.getBlogEntries()});
+});
+ 
+app.get('/about', function(req, res) {
+ res.render('about', {title:"About Me"});
+});
+ 
+app.get('/article/:id', function(req, res) {
+ var entry = blogEngine.getBlogEntry(req.params.id);
+ res.render('article',{title:entry.title, blog:entry});
+});
+ 
+var server = app.listen(8000, function() {
+ console.log('Listening on http://127.0.0.1:8000');
+})
