fd71f191d848414ab4cb8437c20a98109756a8ea
marcia.villalba <marcia.villalba@gmail.com>
2015-05-14 15:06:27 +0300

    Adding Mongo DB using mongoose-promises

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 var express = require('express');
 var app = express();
- 
 var hbs = require('hbs');
-var blogEngine = require('./blog');
+
+var mongoose = require('mongoose-promised').connect('mongodb://localhost/simple');;
 
 app.set('view engine', 'html');
 app.engine('html', hbs.__express);
 app.use(express.bodyParser());
 
+var articleSchema = {
+	id : Number, 
+	title : String, 
+	body : String, 
+	published : String
+};
+
+var Article = mongoose.model('Article', articleSchema, 'articles');
+
 app.get('/', function(req, res) {
- res.render('index',{title:"My Blog", entries:blogEngine.getBlogEntries()});
+	Article.find(function(err, entries) {
+	 	res.render('index',{title:"My Blog", entries:entries});
+	});
 });
  
 app.get('/about', function(req, res) {
@@ -17,10 +28,11 @@ app.get('/about', function(req, res) {
 });
  
 app.get('/article/:id', function(req, res) {
- var entry = blogEngine.getBlogEntry(req.params.id);
- res.render('article',{title:entry.title, blog:entry});
+	Article.where({id : req.params.id}).findOne(function(err, entry) {
+		res.render('article',{title:entry.title, blog:entry});
+	});
 });
  
 var server = app.listen(8000, function() {
- console.log('Listening on http://127.0.0.1:8000');
-})
+ 	console.log('Listening on http://127.0.0.1:8000');
+});
---
+++ data/data.js
@@ -0,0 +1,6 @@
+[{"id":1, "title":"Hello World!", "body":"This is the body of my blog entry. Sooo exciting.", "published":"6/4/2015"},
+{"id":2, "title":"My cat is amazing", "body":"My cat is the most crazy cat in the planet.", "published":"7/4/2015"},
+{"id":3, "title":"Bacon is good for you", "body":"Eat more bacon, it cures cancer!", "published":"16/4/2015"},
+{"id":4, "title":"Node.js is so cool", "body":"Have you realised how fast you build your blog?.", "published":"23/4/2015"},
+{"id":5, "title":"Mongo db coming soon", "body":"In the next posts we will connect mongo db to the blog", "published":"24/4/2015"},
+{"id":6, "title":"Read my blog", "body":"Read it pleaseee and leave comments!", "published":"26/4/2015"}]
