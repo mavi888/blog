f13a2499a0ab9715ccad00ee2c5d8f43e16d4216
marcia.villalba <marcia.villalba@gmail.com>
2015-05-29 21:09:36 +0300

    PAdding the functionality to post a new blog article

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var hbs = require('hbs');
+var moment = require('moment');
 
 var mongoose = require('mongoose-promised').connect('mongodb://localhost/simple');;
 
@@ -18,13 +19,11 @@ var articleSchema = {
 var Article = mongoose.model('Article', articleSchema, 'articles');
 
 app.get('/', function(req, res) {
-	Article.find(function(err, entries) {
-	 	res.render('index',{title:"My Blog", entries:entries});
-	});
+	renderIndex(res);
 });
  
 app.get('/about', function(req, res) {
- res.render('about', {title:"About Me"});
+ 	res.render('about', {title:"About Me"});
 });
  
 app.get('/article/:id', function(req, res) {
@@ -32,7 +31,33 @@ app.get('/article/:id', function(req, res) {
 		res.render('article',{title:entry.title, blog:entry});
 	});
 });
+
+app.post('/add', function(req, res) {
+	var article = new Article ({ 
+		id : randomNumber(), 
+		title : req.body.title,
+		body : req.body.body,
+		published : moment().format('D/M/YYYY')
+	});
+
+	article.saveQ().then(function(){
+        renderIndex(res);
+    }).catch(function(err) {
+    	console.log('There was an error saving the article' + err);
+    });
+});
  
 var server = app.listen(8000, function() {
  	console.log('Listening on http://127.0.0.1:8000');
 });
+
+// Creates a random number between 1 and 9999
+var randomNumber = function() {
+	return Math.floor((Math.random() * 9999) + 1);
+}
+
+var renderIndex = function(res) {
+	Article.find(function(err, entries) {
+ 		res.render('index', {title:"My Blog", entries:entries});
+	});
+}
