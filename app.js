a34ab9e6898abb4c0d980024fd8fcd56e5d5afb0
marcia.villalba <marcia.villalba@gmail.com>
2015-07-20 21:23:53 +0300

    Adding blacktie support

--- app.js
+++ app.js
@@ -5,14 +5,15 @@ var moment = require('moment');
 
 var mongoose = require('mongoose-promised').connect('mongodb://localhost/simple');;
 
+app.use(express.static(__dirname + '/static'));
 app.set('view engine', 'html');
 app.engine('html', hbs.__express);
 app.use(express.bodyParser());
 
 var articleSchema = {
-	id : Number, 
-	title : String, 
-	body : String, 
+	id : Number,
+	title : String,
+	body : String,
 	published : String
 };
 
@@ -21,11 +22,11 @@ var Article = mongoose.model('Article', articleSchema, 'articles');
 app.get('/', function(req, res) {
 	renderIndex(res);
 });
- 
+
 app.get('/about', function(req, res) {
  	res.render('about', {title:"About Me"});
 });
- 
+
 app.get('/article/:id', function(req, res) {
 	Article.where({id : req.params.id}).findOne(function(err, entry) {
 		res.render('article',{title:entry.title, blog:entry});
@@ -33,8 +34,8 @@ app.get('/article/:id', function(req, res) {
 });
 
 app.post('/add', function(req, res) {
-	var article = new Article ({ 
-		id : randomNumber(), 
+	var article = new Article ({
+		id : randomNumber(),
 		title : req.body.title,
 		body : req.body.body,
 		published : moment().format('D/M/YYYY')
@@ -46,7 +47,7 @@ app.post('/add', function(req, res) {
     	console.log('There was an error saving the article' + err);
     });
 });
- 
+
 var server = app.listen(8000, function() {
  	console.log('Listening on http://127.0.0.1:8000');
 });
